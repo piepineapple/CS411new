26d4712059ff49fb6515cde11efadca3e7cbfe1e
Ieva <ievas@bu.edu>
2024-04-13 18:05:00 -0400

    Added packages to server.js and routes.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+//const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
+//mongoose.connect(process.env.MONGODB_URI, {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+//})
+
+//    .then(() => console.log('Connected to MongoDB'))
+//    .catch((err) => console.error('Could not connect to MongoDB:', err));
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Server running at <http://localhost>:${port}
+`);
+});
 
