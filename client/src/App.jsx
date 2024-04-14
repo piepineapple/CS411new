f0961bda59ef9154859a44698b675650d0e4dce7
Shan Ji <jishan@bu.edu>
2024-04-14 00:41:22 -0400

    fixed screen layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
-
-  // Google oAuth 
   const google = window.google;
 
   const handleCallbackResponse = (response) => {
@@ -15,20 +11,18 @@ function App() {
     google.accounts.id.initialize({
       client_id: "358020241759-h8p3kof54voihbcqhqpk7ev2v70ualf4.apps.googleusercontent.com",
       callback: handleCallbackResponse
-    })
+    });
 
     google.accounts.id.renderButton(
       document.getElementById("sign-in-div"),
       { theme: "outline", size: "large" }
-    )
-
+    );
   }, []);
 
   const [inputValue, setInputValue] = useState('');
   const [showPlaylist, setShowPlaylist] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [processedEntry, setProcessedEntry] = useState('');
-
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -69,18 +63,19 @@ function App() {
   };
 
   return (
-    <div id="root" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div className="card" style={{ textAlign: 'center' }}>
-        <h1>Music & Thoughts Generator</h1>
+    <div id="root" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', textAlign: 'center', background: 'linear-gradient(135deg, #FFD700, #ADD8E6, #FFC0CB)' }}>
+      <div className="card" style={{ padding: '20px', maxWidth: '400px', width: '80%', background: 'rgba(255, 255, 255, 0.2)', borderRadius: '10px' }}>
+        <h1 style={{ color: '#333' }}>JournalX</h1>
+        <div id="sign-in-div" style={{ marginBottom: '10px' }}> </div>
         <input
           type="text"
           placeholder="Enter text here"
           value={inputValue}
           onChange={handleInputChange}
-          style={{ margin: '10px' }}
+          style={{ margin: '10px', textAlign: 'center', width: '90%', height: '40px', padding: '10px', background: 'rgba(255, 255, 255, 0.8)', border: 'none', borderBottom: '1px solid #ccc', outline: 'none', borderRadius: '5px', color: '#333', fontSize: '20px', fontWeight: 'bold' }}
         />
         <br />
-        <button onClick={handleEnterButtonClick}>Enter</button>
+        <button onClick={handleEnterButtonClick} style={{ background: 'transparent', border: 'none', padding: '10px 20px', cursor: 'pointer', borderRadius: '5px', color: '#333', fontSize: '18px', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.1)' }}>Enter</button>
         {errorMessage && (
           <p style={{ color: 'red' }}>{errorMessage}</p>
         )}
@@ -91,7 +86,6 @@ function App() {
           </div>
         )}
       </div>
-      <div id="sign-in-div"> </div>
     </div>
   );
 }
