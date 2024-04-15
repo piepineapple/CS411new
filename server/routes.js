7c7f2da2a05b4359d0e2ccbcd2b7df2d5da2c4b2
Ieva <ievas@bu.edu>
2024-04-15 19:32:04 -0400

    Debugging cors issue

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,62 @@
-
-const express = require('express');
+require('dotenv').config();
 const axios = require('axios');
-const journal = require('./models/journal');
+const express = require('express');
+const cors = require('cors');
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+};
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const journal = require('./models/journal');
 const router = express.Router();
+
+
+app.get('/weather', (req, res) => {
+    const city = req.query.city; // access the city sent as a query
+    if (!city) {
+        return res.status(400).json({ error: 'City needed!' });
+    }
+    const apiKey = process.env.Weather_API; // access the API key
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    axios.get(URL)
+        .then((response) => {
+            // obtain only the main weather conditions from the call
+            const condition = response.data.weather[0].main;
+            // console.log('Conditions:', condition);
+        })
+        .catch(error => res.status(500).json({ error: error.toString() }));
+})
+
+
+// fetch weather data for a city
+function fetchWeather(city) {
+    const apiKey = process.env.Weather_API; // access the API key
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    axios.get(URL)
+        .then((response) => {
+            // obtain only the main weather conditions from the call
+            const condition = response.data.weather[0].main;
+            // console.log('Conditions:', condition);
+        })
+        .catch((error) => {
+            console.error('Error fetching the data:', error);
+        });
+}
+
+
+
 router.get('/hn-stories', async (req, res) => {
     // Implementation for fetching stories from Hacker News API
 });
+
 router.post('/save-story', async (req, res) => {
     // Implementation for saving a story to MongoDB
 });
+
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 //const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send("hello");
